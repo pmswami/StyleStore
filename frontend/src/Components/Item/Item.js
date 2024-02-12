e6aee274215936df3b0c580dfb88549682f1c5a8
unknown <usern5949@gmail.com>
2024-02-12 12:55:41 +0530

    Added popular products

--- frontend/src/Components/Item/Item.js
+++ frontend/src/Components/Item/Item.js
@@ -6,8 +6,9 @@ export const Item = (props) => {
     <div className="item">
       <img src={props.image} alt="The props image" />
       <p>{props.name}</p>
-      <div className="item-prices">{props.new_price}</div>
-      <div className="item-price-old">{props.old_price}</div>
+      <div className="item-price-new">${props.new_price}</div>
+      <div className="item-price-old">${props.old_price}</div>
     </div>
   );
 };
+ 
