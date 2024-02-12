c6834f79bc8724d23e0cd54df7342d18485cebfd
unknown <usern5949@gmail.com>
2024-02-12 22:55:10 +0530

    Added login page with styles

--- frontend/src/Pages/LoginSignup.js
+++ frontend/src/Pages/LoginSignup.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./CSS/LoginSignup.css";
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Signup</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email ID" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account?<span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
